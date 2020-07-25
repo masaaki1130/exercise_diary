0d9e6a750033b6eb8f63af55fecb1d3dcdc94383
masaaki1130 <masaki11maki30@gmail.com>
2020-07-25 18:52:25 +0900

    prefectureadd

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+
+  def new
+    super
+  end
+
   def create
     @user = User.new(sign_up_params)
     unless @user.valid?
@@ -11,8 +17,25 @@ class Users::RegistrationsController < Devise::RegistrationsController
     session["devise.regist_data"] = {user: @user.attributes}
     session["devise.regist_data"][:user]["password"] = params[:user][:password]
     @address = @user.build_address
-    render :new_address
+    render :new_destinations
    end
+
+   def new_address
+   end
+
+   def create_address
+    @user = User.new(session["devise.regist_data"]["user"])
+    @address = Address.new(address_params)
+    unless @address.valid?
+      flash.now[:alert] = @address.errors.full_messages
+      render :new_destinations and return
+    end
+    @user.build_address(@address.attributes)
+    @user.save
+    sign_in(:user, @user)
+    redirect_to root_path
+  end
+  
    
    protected
    
@@ -20,4 +43,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
      devise_parameter_sanitizer.permit(:sign_up, keys:
                            [:nickname])
    end
+   def address_params
+    params.require(:address).permit(:prefecture_id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :diaries
   has_many :comments
+  has_one :address
 end
