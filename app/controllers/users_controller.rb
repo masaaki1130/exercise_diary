5951d95e3c0b75f4b6a36458d2a2b76b4375064d
masaaki1130 <masaki11maki30@gmail.com>
2020-07-27 11:11:04 +0900

    weatheredit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @nickname = user.nickname
     @diaries = user.diaries.order("created_at DESC")
+    @destination = Address.find(current_user.id)
   end
 end
