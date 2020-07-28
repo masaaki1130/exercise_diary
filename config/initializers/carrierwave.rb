6fe025628f593bf3e53f07d2c1eb3b94d4672a29
masaaki1130 <masaki11maki30@gmail.com>
2020-07-28 18:37:36 +0900

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,20 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_ACCESS_KEY_ID"],
-    region: 'ap-northeast-1'
-  }
+  if Rails.env.production? || Rails.env.development?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_ACCESS_KEY_ID"],
+      region: 'ap-northeast-1'
+    }
 
-  config.fog_directory  = 'exercisediary2020'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/exercisediary2020'
+    config.fog_directory  = 'exercisediary2020'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/exercisediary2020'
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
 end
