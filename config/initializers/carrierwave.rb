407c0e12b983ed96936c1a3e8109b39a9de71eb0
masaaki1130 <masaki11maki30@gmail.com>
2020-07-28 18:55:39 +0900

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,8 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   if Rails.env.production? || Rails.env.development? 
+    config.storage :file
+  elseif Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_public = false 
@@ -16,8 +18,5 @@ CarrierWave.configure do |config|
 
     config.fog_directory  = 'exercisediary2020'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/exercisediary2020'
-  else
-    config.storage :file
-    config.enable_processing = false if Rails.env.test?
   end
 end
