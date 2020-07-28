0ec29d98ccfdc646462f6715db75b486d63ca46c
masaaki1130 <masaki11maki30@gmail.com>
2020-07-28 19:35:23 +0900

    fix

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -10,12 +10,7 @@ class DiariesController < ApplicationController
   end
 
   def create
-    @diary = Diary.create(diary_params)
-    if @diary.save
-      render :create
-    else
-      render :new
-    end
+    Diary.create(diary_params)
   end
 
   def destroy
